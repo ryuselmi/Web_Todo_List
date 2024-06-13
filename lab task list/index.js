dc02a699b18b569465b67b58f70be1ff4363a869
ryuselmi <yuselmirodriguez24@gmail.com>
2024-06-13 16:08:12 -0700

    First Commit

--- lab task list/index.js
+++ lab task list/index.js
@@ -1,2 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const todoFrom = document.getElementById("todo-form");
+  const todoList = document.getElementById("todo-list");
+  todoFrom.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const text = document.getElementById("todo-input").value;
+    addTodo(text);
+  });
+  /**
+   *
+   * @param {string} str // add a new string value to the list
+   */
+  function addTodo(str) {
+    const li = document.createElement("li"); // add you own
+    li.textContent = str;
 
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.className = ".removeButton";
 
+    removeButton.addEventListener("click", () => {
+      todoList.removeChild(li);
+    });
+    li.appendChild(removeButton);
+    todoList.appendChild(li);
+  }
+});
